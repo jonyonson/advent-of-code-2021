451f6452dbe2bb3d9be1bffc2ae2a09194b30eba
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2021-12-03 07:17:05 -0600

    it works but wow thats bad

--- 03/index.js
+++ 03/index.js
@@ -2,28 +2,24 @@ const path = require('path');
 const getInput = require('../getInput');
 
 const inputFile = path.resolve(__dirname, 'input.txt');
-// const data = getInput(inputFile);
+const data = getInput(inputFile);
 
-let data = [
-  '00100',
-  '11110',
-  '10110',
-  '10111',
-  '10101',
-  '01111',
-  '00111',
-  '11100',
-  '10000',
-  '11001',
-  '00010',
-  '01010',
-];
+// let data = [
+//   '00100',
+//   '11110',
+//   '10110',
+//   '10111',
+//   '10101',
+//   '01111',
+//   '00111',
+//   '11100',
+//   '10000',
+//   '11001',
+//   '00010',
+//   '01010',
+// ];
 
 function partOne() {
-  // create an array of n 0s
-  // for each line,
-  //   if the line is a comment, skip it
-  //   if the line is a variable assignment
   let oneBitsCount = new Array(data[0].length).fill(0);
 
   for (let i = 0; i < data.length; i++) {
@@ -33,9 +29,6 @@ function partOne() {
       }
     }
   }
-
-  // return oneBitsCount;
-  // return data.length / 2;
 
   const gammaRate = oneBitsCount
     .map((count) => {
@@ -53,29 +46,53 @@ function partOne() {
 }
 
 function partTwo() {
-  // oxygen generator rating
-  // find the most common value in the current bit position
-  // keep only numbers with that bit in that position
-  // tie goes to 1
-
   let arr = data;
+  let count = 0;
+  let oneBitsCount;
 
   while (arr.length > 1) {
-    let oneBitsCount = 0;
+    oneBitsCount = 0;
 
     for (let i = 0; i < arr.length; i++) {
-      console.log(Boolean(+arr[i][0]));
-      if (+arr[i][0]) {
+      if (+arr[i][count]) {
         oneBitsCount += 1;
       }
     }
 
     if (oneBitsCount >= arr.length / 2) {
-      arr.filter((x) => x[0] === '1');
+      arr = arr.filter((x) => x[count] === '1');
+    } else {
+      arr = arr.filter((x) => x[count] === '0');
     }
+
+    count++;
   }
 
-  return oneBitsCount;
+  let arr1 = data;
+  count = 0;
+
+  while (arr1.length > 1) {
+    oneBitsCount = 0;
+
+    for (let i = 0; i < arr1.length; i++) {
+      if (+arr1[i][count]) {
+        oneBitsCount += 1;
+      }
+    }
+
+    if (oneBitsCount >= arr1.length / 2) {
+      arr1 = arr1.filter((x) => x[count] === '0');
+    } else {
+      arr1 = arr1.filter((x) => x[count] === '1');
+    }
+
+    count++;
+  }
+
+  const oxygenGeneratorRating = parseInt(arr.join(''), 2);
+  const c02ScrubberRating = parseInt(arr1.join(''), 2);
+
+  return oxygenGeneratorRating * c02ScrubberRating;
 }
 
 module.exports = { partOne, partTwo };

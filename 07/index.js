ad736f3e683a6937a2327842b06215b22f24498b
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2021-12-06 18:35:09 -0600

    scaffold day 7

---
+++ 07/index.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const getInput = require('../getInput');
+
+const inputFile = path.resolve(__dirname, 'input.txt');
+const data = getInput(inputFile);
+
+function partTwo() {
+  return 'yo';
+}
+
+function partTwo() {
+  return 'yo';
+}
+
+module.exports = { partOne, partTwo };
